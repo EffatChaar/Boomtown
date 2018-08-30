f88f976b17d52c13064f8c893394686a10ee5094
Effat Chaar <effat.shaar@gmail.com>
2018-08-29 17:27:09 -0700

    added proptypes and viewerContext to ShareItemPreview page

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ItemCard from '../ItemCard'
 import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core'
+import { ViewerContext } from '../../context/ViewerProvider'
+import PropTypes from 'prop-types'
+import styles from './styles'
+
 
 const ShareItemPreview = props => {
-  console.log(props)
-  return <ItemCard item={props.shareItemPreview} />
+  <ViewerContext.Consumer>
+    {({ viewer }) => {
+      props.shareItemPreview.itemowner = {
+        fullname: viewer.fullname,
+        email: viewer.email
+      }
+      return <ItemCard item={props.shareItemPreview} />
+    }}
+    </ViewerContext.Consumer>
 }
 
 const mapStateToProps = state => {
@@ -13,4 +25,19 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(ShareItemPreview)
+ShareItemPreview.propTypes = {
+  classes: PropTypes.object.isRequired,
+  shareItemPreview: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    tags: PropTypes.array.isRequired,
+    imageurl: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    created: PropTypes.instanceOf(Date).isRequired,
+    itemowner: PropTypes.shape({
+      fullname: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired
+    }).isRequired
+  })
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(ShareItemPreview))
